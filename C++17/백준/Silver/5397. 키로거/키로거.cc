e62e036b66fba031628f685d7d09588392789b4b
OK-SW <144411938+okseonwoo@users.noreply.github.com>
2025-01-13 20:55:30 +0900

    Update 키로거.cc

--- C++17/백준/Silver/5397. 키로거/키로거.cc
+++ C++17/백준/Silver/5397. 키로거/키로거.cc
@@ -7,25 +7,24 @@ int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int t; cin >> t;
+  int t; 
+  cin >> t;
 
   while (t--){
     list<char> lis;
     list<char>::iterator iter = lis.begin();
-    string s; cin >> s;
+    string s; 
+    cin >> s;
 
     for (auto c : s){
       if (c == '-'){
-        if (iter != lis.begin())
-          iter = lis.erase(--iter);
+        if (iter != lis.begin()) iter = lis.erase(--iter);
       }
       else if (c == '<'){
-        if (iter != lis.begin())
-          iter--;
+        if (iter != lis.begin()) iter--;
       }
       else if (c == '>'){
-        if (iter != lis.end())
-          iter++;
+        if (iter != lis.end()) iter++;
       }
       else
         lis.insert(iter, c);
